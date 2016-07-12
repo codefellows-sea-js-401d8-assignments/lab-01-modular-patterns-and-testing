a885f4a2066e7d8711cd2a21d2468a97690cd8a5
Aliza Pilisuk <apilisuk@gmail.com>
2016-07-11 20:23:10 -0700

    fixed cow

--- lab-aliza/lib/cowsay.js
+++ lab-aliza/lib/cowsay.js
@@ -1,3 +1,15 @@
 module.exports = function cowsay(string){
-  return string;
+
+  const cow = '\n        \\   ^__^\n' +
+  '         \\  (oo)\\_______ \n' +
+  '            (__)\\       )\\/\\\n' +
+  '                ||----w |\n' +
+  '                ||     ||\n';
+
+  const bubbleTopBottom = '-'.repeat(string.length + 2);
+
+  const bubbleString = string;
+
+  console.log(' ' + bubbleTopBottom + '\n| ' + bubbleString + ' |\n' + ' ' + bubbleTopBottom + cow);
+
 };
--- lab-aliza/test/cowsay-test.js
+++ lab-aliza/test/cowsay-test.js
@@ -3,6 +3,18 @@ const expect = require('chai').expect;
 
 describe('the cowsay function', function(){
   it('should display cow saying inputed string', function(){
-    expect(cowsay('hello world')).to.eql('hello world');
+    expect(cowsay('hello world')).to.eql(console.log(' ' +
+      '-'.repeat('hello world'.length + 2) +
+      '\n| ' +
+      'hello world' +
+      ' |\n' +
+      ' ' +
+      '-'.repeat('hello world'.length + 2) +
+      '\n        \\   ^__^\n' +
+      '         \\  (oo)\\_______ \n' +
+      '            (__)\\       )\\/\\\n' +
+      '                ||----w |\n' +
+      '                ||     ||\n'
+    ));
   });
 });
